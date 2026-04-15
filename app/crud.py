28fa11fd14b77c263110fc4e87c3405d496af6ea
vanivietquat <dinhhuy25102004@gmail.com>
2026-04-15 10:37:15 +0700

    validate

--- app/crud.py
+++ app/crud.py
@@ -3,10 +3,10 @@ from . import models, schemas
 
 
 def get_student(db: Session, student_id: int):
-    return db.query(models.Student).all
+    return db.query(models.Student).all()
 
 def get_students(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Student).all
+    return db.query(models.Student).all()
 
 def create_student(db: Session, student: schemas.StudentCreate):
     db_student = models.Student(
